2982f5813946e21a0c37580bcacdb4a2c7f0e836
Xavier Arce <xavierarce54321@gmail.com>
2025-07-10 12:46:50 +0200

    Refactor user handling by consolidating UserRepository methods into User class

--- chatweb/service/UserService.php
+++ chatweb/service/UserService.php
@@ -1,19 +1,19 @@
 <?php
-require_once __DIR__ . '/../model/UserRepository.php';
+require_once __DIR__ . '/../model/User.php';
 
 class UserService {
     public static function login($login, $mdp) {
-        $user = UserRepository::findByLogin($login);
+        $user = User::findByLogin($login);
         if ($user && password_verify($mdp, $user['mdp'])) {
             return $user;
         }
         return null;
     }
     public static function register($pseudo, $login, $mdp, $email) {
-        if (UserRepository::findByLogin($login)) {
+        if (User::findByLogin($login)) {
             return 'Login déjà utilisé';
         }
-        UserRepository::create($pseudo, $login, password_hash($mdp, PASSWORD_DEFAULT), $email);
+        User::create($pseudo, $login, password_hash($mdp, PASSWORD_DEFAULT), $email);
         return true;
     }
 }
